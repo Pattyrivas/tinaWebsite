22b46e478a7bff654b78a6bb037ce790ae32f8e4
Carlos Lopez <carlosmlopez066@gmail.com>
2024-02-21 12:27:51 -0400

    implementacion de api para el formilario

--- src/Components/Contactame/index.jsx
+++ src/Components/Contactame/index.jsx
@@ -1,18 +1,36 @@
+import { contactFormAPI } from "../../helper/contacFormAPI"
 const Contactame = () => {
+  const handleSubmit = async (event) => {
+    const myform = document.getElementById("form")
+    console.log('evento: ', event)
+    event.preventDefault()
+    const form = new FormData(event.target)
+    const response = await fetch(event.target.action, {
+      method: myform.method,
+      body: form,
+      headers: {
+        "accept": "application/json"
+      }
+    })
+    if (response.ok) {
+      myform.reset()
+      alert("Gracias por contactarme")
+    }
+  }
   return (
     <div id="contactame" className="grid grid-cols-2 m-10">
       <div className="flex flex-col items-center justify-center">
-        <form action="" className=" flex flex-col gap-3">
+        <form id="form" action="https://formspree.io/f/xvoeajda" method="POST" onSubmit={handleSubmit} className=" flex flex-col gap-3">
           <p
             className="ubuntu-medium-italic text-pink-500 text-3xl"
-          >Contactame</p>
-          <label for="nombre" className="ubuntu-medium text-pink-700 text-xl">Nombre</label>
-          <input for="nombre" type="text" placeholder="Nombre y apellido..." className="border border-pink-200 rounded-lg w-80 h-8 p-2 focus:outline-pink-700" />
-          <label for="Email" className="ubuntu-medium text-pink-700 text-xl">Email</label>
-          <input for="Email" type="email" placeholder="Direccion de correo" className="border border-pink-200 rounded-lg w-80 h-8 p-2 focus:outline-pink-700" />
-          <label for="Mensaje" className="ubuntu-medium text-pink-700 text-xl">Mensaje</label>
-          <textarea for="Mensaje" type="text" placeholder="Mensaje..." className="border border-pink-200 rounded-lg w-80 h-14 p-2 focus:outline-pink-700" />
-          <button className="bg-pink-500 text-white ubuntu-bold rounded-lg h-10 mt-3 hover:bg-pink-400">Enviar</button>
+          >Contáctame</p>
+          <label htmlFor="nombre" className="ubuntu-medium text-pink-700 text-xl">Nombre</label>
+          <input htmlFor="nombre" type="text" name="nickname" placeholder="Nombre y apellido..." className="border border-pink-200 rounded-lg w-80 h-8 p-2 focus:outline-pink-700" />
+          <label htmlFor="Email" aria-required className="ubuntu-medium text-pink-700 text-xl">Email</label>
+          <input htmlFor="Email" type="email" name="email" placeholder="Direccion de correo" className="border border-pink-200 rounded-lg w-80 h-8 p-2 focus:outline-pink-700" />
+          <label htmlFor="Mensaje" className="ubuntu-medium text-pink-700 text-xl">Mensaje</label>
+          <textarea htmlFor="Mensaje" type="text" name="message" placeholder="Mensaje..." className="border border-pink-200 rounded-lg w-80 h-14 p-2 focus:outline-pink-700" />
+          <button type="submit" className="bg-pink-500 text-white ubuntu-bold rounded-lg h-10 mt-3 hover:bg-pink-400">Enviar</button>
         </form>
       </div>
       <div>
