db02224dea82f0945280e436638887abf18d5b35
Carlos Lopez <carlosmlopez066@gmail.com>
2024-02-26 11:06:58 -0400

    reajuste de dise;o en componente portafolio

--- src/Components/Portafolio/index.jsx
+++ src/Components/Portafolio/index.jsx
@@ -1,66 +1,79 @@
 const Portafolio = () => {
   return (
-    <section id="portafolio" className="bg-pink-100 flex flex-col items-center justify-center">
+    <section id="portafolio" className="bg-pink-100 flex flex-col items-center justify-center pb-20">
       <div>
         <p className="w-full ubuntu-bold-italic mt-28 text-5xl text-pink-500">Valen Creative</p>
         <p className="w-full ubuntu-ligth-italic  text-2xl text-pink-500">GRAPHIC DESIGN PORTFOLIO</p>
       </div>
-      <div className=" grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen=lg  h-screen  p-10">
+      <div className=" grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 w-full max-w-screen=lg  h-screen  p-10 pr-56 pl-56">
         <div>
           <img
             src="https://www.esneca.com/wp-content/uploads/que-es-diseno-grafico-1200x900.jpg"
             alt="proyecto valen"
-            className="w-full rounded-lg cursor-pointer"
+            className="w-full rounded-md cursor-pointer"
           />
-          <h3 className="ubuntu-medium-italic text-lg text-pink-500">
+          <h3 className="ubuntu-bold-italic text-lg text-pink-500 pl-2 pt-2">
             Proyecto valen
           </h3>
-          <p className="ubuntu-regular text-white">descripcion del proyecto bla bla bla...</p>
+          <p className="ubuntu-lingth text-pink-500 pl-2">descripcion del proyecto bla bla bla... bla bla bla bla bla bla bla bla bla bla bla bla</p>
         </div>
         <div>
           <img
             src="https://www.esneca.com/wp-content/uploads/que-es-diseno-grafico-1200x900.jpg"
             alt="proyecto valen"
-            className="w-full rounded-lg cursor-pointer"
+            className="w-full rounded-md cursor-pointer"
           />
-          <h3 className="ubuntu-medium-italic text-lg text-pink-500">
+          <h3 className="ubuntu-bold-italic text-lg text-pink-500 pl-2 pt-2">
             Proyecto valen
           </h3>
-          <p className="ubuntu-regular text-white">descripcion del proyecto bla bla bla...</p>
+          <p className="ubuntu-lingth text-pink-500 pl-2">descripcion del proyecto bla bla bla... bla bla bla bla bla bla bla bla bla bla bla bla</p>
         </div>
         <div>
           <img
             src="https://www.esneca.com/wp-content/uploads/que-es-diseno-grafico-1200x900.jpg"
             alt="proyecto valen"
-            className="w-full rounded-lg cursor-pointer"
+            className="w-full rounded-md cursor-pointer"
           />
-          <h3 className="ubuntu-medium-italic text-lg text-pink-500">
+          <h3 className="ubuntu-bold-italic text-lg text-pink-500 pl-2 pt-2">
             Proyecto valen
           </h3>
-          <p className="ubuntu-regular text-white">descripcion del proyecto bla bla bla...</p>
+          <p className="ubuntu-lingth text-pink-500 pl-2">descripcion del proyecto bla bla bla... bla bla bla bla bla bla bla bla bla bla bla bla</p>
         </div>
         <div>
           <img
             src="https://www.esneca.com/wp-content/uploads/que-es-diseno-grafico-1200x900.jpg"
             alt="proyecto valen"
-            className="w-full rounded-lg cursor-pointer"
+            className="w-full rounded-md cursor-pointer"
           />
-          <h3 className="ubuntu-medium-italic text-lg text-pink-500">
+          <h3 className="ubuntu-bold-italic text-lg text-pink-500 pl-2 pt-2">
             Proyecto valen
           </h3>
-          <p className="ubuntu-regular text-white">descripcion del proyecto bla bla bla...</p>
+          <p className="ubuntu-lingth text-pink-500 pl-2">descripcion del proyecto bla bla bla... bla bla bla bla bla bla bla bla bla bla bla bla</p>
         </div>
         <div>
           <img
             src="https://www.esneca.com/wp-content/uploads/que-es-diseno-grafico-1200x900.jpg"
             alt="proyecto valen"
-            className="w-full rounded-lg cursor-pointer"
+            className="w-full rounded-md cursor-pointer"
           />
-          <h3 className="ubuntu-medium-italic text-lg text-pink-500">
+          <h3 className="ubuntu-bold-italic text-lg text-pink-500 pl-2 pt-2">
             Proyecto valen
           </h3>
-          <p className="ubuntu-regular text-white">descripcion del proyecto bla bla bla...</p>
+          <p className="ubuntu-lingth text-pink-500 pl-2">descripcion del proyecto bla bla bla... bla bla bla bla bla bla bla bla bla bla bla bla</p>
         </div>
+        <div>
+          <img
+            src="https://www.esneca.com/wp-content/uploads/que-es-diseno-grafico-1200x900.jpg"
+            alt="proyecto valen"
+            className="w-full rounded-md cursor-pointer"
+          />
+          <h3 className="ubuntu-bold-italic text-lg text-pink-500 pl-2 pt-2">
+            Proyecto valen
+          </h3>
+          <p className="ubuntu-lingth text-pink-500 pl-2">descripcion del proyecto bla bla bla... bla bla bla bla bla bla bla bla bla bla bla bla</p>
+        </div>
+
+
       </div>
 
 
